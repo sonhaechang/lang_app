969b109fcb5767fb7c7d168d63fcec6f061ae6ef
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-24 03:34:58 +0900

    refector: Card Project part Two
    1. 기존 코드 정리
    2. onPanResponderRelease에서 좌우 드래그시 특정 범위 넘어가면 Card가 사라지게 코드 추가

--- App.js
+++ App.js
@@ -25,47 +25,67 @@ const Card = styled(Animated.createAnimatedComponent(View))`
 `;
 
 export default function App() {
-	const panResponder = useRef(
-		PanResponder.create({
-			onStartShouldSetPanResponder: () => true,
-			onPanResponderMove: (_, { dx }) => {
-        		position.setValue(dx);
-			},
-			onPanResponderGrant: () => onPressIn(),
-			onPanResponderRelease: () => {
-				Animated.parallel([
-					onPressOut,
-					Animated.spring(position, {
-						toValue: 0,
-						useNativeDriver: true,
-					})
-				]).start();
-			},
-		})
-	).current;
-
 	const scale = useRef(new Animated.Value(1)).current;
-
 	const position = useRef(new Animated.Value(0)).current;
 
-	const onPressIn = () => (
-		Animated.spring(scale, { 
-			toValue: 0.95,
-			useNativeDriver: true, 
-		}).start()
-	);
+	const rotation = position.interpolate({
+		inputRange: [-250, 250],
+		outputRange: ['-15deg', '15deg'],
+	});
+
+	const onPressIn = Animated.spring(scale, { 
+		toValue: 0.95,
+		useNativeDriver: true, 
+	});
 
 	const onPressOut = Animated.spring(scale, { 
 		toValue: 1, 
 		useNativeDriver: true, 
 	});
 
+	const goCenter = Animated.spring(position, { 
+		toValue: 0, 
+		useNativeDriver: true, 
+	});
+
+	const panResponder = useRef(
+		PanResponder.create({
+			onStartShouldSetPanResponder: () => true,
+			onPanResponderMove: (_, { dx }) => {
+        		position.setValue(dx);
+			},
+			onPanResponderGrant: () => onPressIn.start(),
+			onPanResponderRelease: (_, { dx }) => {
+				if (dx < -320) {
+					Animated.spring(position, {
+						toValue: -500, 
+						useNativeDriver: true
+					}).start();
+				} else if (dx > 320) {
+					Animated.spring(position, {
+						toValue: 500, 
+						useNativeDriver: true
+					}).start();
+				} else {
+					Animated.parallel([
+						onPressOut, 
+						goCenter,
+					]).start();
+				}
+			},
+		})
+	).current;
+
 	return (
 		<Container>
 			<Card 
 				{...panResponder.panHandlers}
 				style={{
-					transform: [{ scale }, { translateX: position }],
+					transform: [
+						{ scale }, 
+						{ translateX: position },
+						{ rotateZ: rotation },
+					],
 				}}
 			>
 				<Ionicons name='pizza' color='#192a56' size={98} />
