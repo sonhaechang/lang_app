7568f32659ef0064c3c930866f38aecaf94c45e5
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-23 15:40:30 +0900

    feat: Manual Animations
    1. react-native의 Animated API를 사용하지 않고 style의 transform으로 같은(기본적인) 기능 구현 해보기
    2. styled-components 자동완성을 위해서 @types/styled-components, @types/styled-components-react-native 설치

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styled from 'styled-components/native';
 
 
+const Container = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Box = styled.TouchableOpacity`
+	background-color: tomato;
+	width: 200px;
+	height: 200px;
+`;
+
 export default function App() {
+	const [y, setY] = useState(0);
+	const [intervalId, setIntervalId] = useState(null);
+
+	const mvoeUp = () => {
+		const id = setInterval(() => setY(prev => prev+1), 1);
+		setIntervalId(id)
+	}
+
+	useEffect(() => {
+		if (y === 200) { clearImmediate(intervalId); }
+	}, [y, intervalId]);
+
 	return (
-		null
+		<Container>
+			<Box 
+				onPress={mvoeUp} 
+				style={{
+					transform: [{ translateY: y }],
+				}}
+			/>
+		</Container>
 	);
 }
